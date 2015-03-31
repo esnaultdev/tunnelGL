dab294561198259c94b1725ebccccf48dc3c770f
Goutye <gaut.b@sfr.fr>
2015-03-31 13:55:08 +0200

    Engine: Make_triangle && program

--- include/Engine.hpp
+++ include/Engine.hpp
@@ -6,7 +6,7 @@
 class Engine {
 
 public:
-	Engine();
+	Engine(glhf::Program prog);
 	~Engine();
 	void update(double currentTime);
 	void draw();
@@ -16,6 +16,7 @@ private:
 	Screen *_currentScreen;
 	Screen *_nextScreen = 0;
 	double _prevTime = 0;
+	glhf::Program _prog;
 };
 
 #endif
